e2593f1b2579da1b3af9785ecab133960d1f9f65
Mitchell Thompson <leonsbuddydave@gmail.com>
2022-08-15 01:13:22 -0700

    Adding more types to WriterOptions

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,25 @@ export type MeshWriterOptions = { [key: string]: any }
 
 export function MeshWriter(scene: Scene, options: MeshWriterOptions ): Writer;
 
+export type WriterOptions = { 
+    "font-family"?: string;
+    "letter-height"?: string;
+    "letter-thickness"?: string;
+    color?: string;
+    anchor?: string;
+    colors?: {
+        diffuse?: string;
+        specular?: string;
+        ambient?: string;
+        emissive?: string;
+    },
+    position?: {
+        x: number;
+        y: number;
+        z: number;
+    }
+};
+
 export class Writer {
-    constructor( scene: Scene, options: { [key: string]: any } );
+    constructor( text: string, options: WriterOptions );
 }
